4e990f51a2ea1ed9ff2aac69e843039d4e3a1aa3
ChaniAsh <99870470+ChaniAsh@users.noreply.github.com>
2022-05-26 14:24:45 +0300

    Add files via upload

---
+++ Bll/ConnectDb.cs
@@ -0,0 +1,77 @@
+﻿using Dal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dto;
+
+namespace Bll
+{
+    public class ConnectDb
+    {
+        Entities db = new Entities();
+        public List<Task_Dto> GetAllTasks()
+        {
+
+            List<Dto.Task_Dto> lst = new List<Dto.Task_Dto>();
+            foreach (var item in db.Task.ToList())
+            {
+                lst.Add(Task_Dto.DaltoDto(item));
+            }
+
+            return lst;
+        }
+
+        public List<Position_Dto> GetAllPositions()
+        {
+            List<Position_Dto> lst = new List<Position_Dto>();
+            foreach (var item in db.Position.ToList())
+            {
+                lst.Add(Position_Dto.DaltoDto(item));
+            }
+            return lst;
+        }
+
+        public void AddPosition(Position_Dto position)
+        {
+            DalInstance.db.Position.Add(position.DtoToDal());
+            DalInstance.db.SaveChanges();
+        }
+        public void AddTask(Task_Dto task)
+        {
+            DalInstance.db.Task.Add(task.DtoToDal());
+            DalInstance.db.SaveChanges();
+        }
+        //public Task_Dto GetCustomerById(string id)
+        //{
+        //    Dto.Task_Dto customer = new Dto.Task_Dto();
+        //    List<Dto.Task_Dto> lst = GetAllCustomers();
+        //    foreach (var item in lst)
+        //    {
+        //        if (item.id == id)
+        //        {
+        //            customer = item;
+        //        }               
+        //    }
+        //    return customer;
+        //}
+
+        //public Task_Dto GetCustomerByName(string name)
+        //{
+        //    Dto.Task_Dto customer = new Dto.Task_Dto();
+        //    List<Dto.Task_Dto> lst = GetAllCustomers();
+        //    foreach (var item in lst)
+        //    {
+        //        if (item.name == name)
+        //        {
+        //            customer = item;
+        //        }
+        //    }
+        //    return customer;
+        //}
+
+
+
+    }
+}
---
+++ Dal/DalInstance.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Dal
+{
+    public class DalInstance
+    {
+        public static Entities db = new Entities();
+        public void Save()
+        {
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+    }
+}
